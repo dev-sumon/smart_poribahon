780f623ac4829f431638d731128918feed237db9
Md. Syful islam <92030793+syful2021@users.noreply.github.com>
2024-11-05 13:26:26 +0600

    stand_intro section
     stand_intro section

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ document.querySelectorAll('.faq-question').forEach(question => {
     });
 });
 // FAQ section JS end
+
+
+
+    const video = document.getElementById('video');
+    const playButton = document.getElementById('playButton');
+
+    playButton.addEventListener('click', () => {
+        if (video.paused) {
+            video.play();
+            playButton.style.display = 'none'; // Hide button when video is playing
+        } else {
+            video.pause();
+            playButton.style.display = 'flex'; // Show button when video is paused
+        }
+    });
+
+    video.addEventListener('pause', () => {
+        playButton.style.display = 'flex';
+    });
+
+    video.addEventListener('play', () => {
+        playButton.style.display = 'none';
+    });
